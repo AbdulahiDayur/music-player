bfdc808869fe5fd8673846302eafc752428e6cfc
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-03-14 22:41:14 -0700

    Update script.js

--- music/script.js
+++ music/script.js
@@ -119,6 +119,7 @@ function setProgressBar(e) {
 
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong() ));
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong)
 prevBtn.addEventListener('click', prevSong);
 music.addEventListener('timeupdate', updateProgressBar);
 // progressContainer.addEventListener('click', setProgressBar)
