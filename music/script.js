f542aaeb60ad2502bf8f2425b0a5b655e5cc8feb
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-03-14 20:03:16 -0700

    Update progress bar width

--- music/script.js
+++ music/script.js
@@ -1,11 +1,32 @@
 const image = document.querySelector('img');
 const title = document.getElementById("title");
-const artist = document.getElementById("artist");
 
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
+
+// Music 
+const songs = [
+  {
+    name: 'jacinto-1',
+    displayName: "Electric Chill Machine"
+  },
+  {
+    name: 'jacinto-2',
+    displayName: "Seven Nation Army (Remix)"
+  },
+  {
+    name: 'jacinto-3',
+    displayName: "Psycho Tommy & His Mom"
+  },
+  {
+    name: 'metric-1',
+    displayName: "Safe Haven, Holly Hell"
+  }
+];
 
 // Check if Playing 
 let isPlaying = false;
@@ -14,7 +35,7 @@ let isPlaying = false;
 function playSong() {
   isPlaying = true;
   playBtn.classList.replace('fa-play', 'fa-pause');
-  playBtn.setAttribute('title', 'Pause')
+  playBtn.setAttribute('title', 'Pause');
   music.play()
 }
 
@@ -22,33 +43,9 @@ function playSong() {
 function pauseSong() {
   isPlaying = false;
   playBtn.classList.replace('fa-pause', 'fa-play');
-  playBtn.setAttribute('title', 'Play')
+  playBtn.setAttribute('title', 'Play');
   music.pause();
 }
-
-// Music 
-const songs = [
-  {
-    name: 'jacinto-1',
-    displayName: "Electric Chill Machine",
-    artist: 'Jacinto Design'
-  },
-  {
-    name: 'jacinto-2',
-    displayName: "Seven Nation Army (Remix)",
-    artist: 'Jacinto Design'
-  },
-  {
-    name: 'jacinto-3',
-    displayName: "Psycho Tommy & His Mom",
-    artist: 'Jacinto Design'
-  },
-  {
-    name: 'metric-1',
-    displayName: "Safe Haven, Holly Hell",
-    artist: 'Jacinto Design'
-  }
-];
 
 // Update DOM
 function loadSong(song) {
@@ -66,22 +63,31 @@ function prevSong() {
   if (songIndex < 0) {
     songIndex = songs.length - 1;
   }
-  loadSong(songs[songIndex])
-  playSong()
-  console.log("songIndex = ", songIndex);
+  loadSong(songs[songIndex]);
+  playSong();
 }
 
 // Next Song
 function nextSong() {
   songIndex++;
   if (songIndex > songs.length -1) {
-    songIndex = 0
+    songIndex = 0;
   }
   loadSong(songs[songIndex]);
-  playSong()
-  console.log("songIndex = ", songIndex);
+  playSong();
+}
+
+// Update Progress Bar & Time
+function updateProgressBar(e) {
+  if (isPlaying) {
+    const {duration, currentTime} = e.srcElement;
+    // Update progress bar width
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`
+  }
 }
 
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong() ));
 nextBtn.addEventListener('click', nextSong);
 prevBtn.addEventListener('click', prevSong);
+music.addEventListener('timeupdate', updateProgressBar);
